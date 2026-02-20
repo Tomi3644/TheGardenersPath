f3dde67ac7ac207fe9fd5e39a34cf488e63f5d8a
Thomas B <thomas.barlier.auge@gmail.com>
2026-02-20 14:05:59 +0100

    Ajout du script LootAtTarget
    Script qui permet lorsqu'il est appliqué à un objet de le faire regarder la caméra principale

---
+++ Assets/Scripts/LookAtTarget.cs
@@ -0,0 +1,24 @@
+using System;
+using UnityEngine;
+
+public class LookAtTarget : MonoBehaviour
+{
+    private Transform target;
+
+    void Awake()
+    {
+        // Every objects affected by LookatTarget look at the camera
+        target = Camera.main.transform;
+    }
+
+    void Update()
+    {
+        if(target != null)
+        {
+            // Inverted direction for the quads
+            Vector3 direction = target.position - transform.position;
+            direction.y = 0f;
+            transform.rotation = Quaternion.LookRotation(-direction);
+        }
+    }
+}
