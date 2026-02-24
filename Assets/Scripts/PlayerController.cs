397108ed857a6980b6eab5d60e43541a2b1eec47
Thomas B <thomas.barlier.auge@gmail.com>
2026-02-24 16:46:52 +0100

    Added bouncer
    Added bouncer in the layers (6) and in the code

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
     private CharacterController controller;
     private Vector3 playerVelocity;
     private bool isGrounded;
+    private bool isBouncing;
     private bool isJumping;
     private InputManager inputManager;
     private Transform cameraTransform;
@@ -33,7 +34,9 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        isGrounded = Physics.SphereCast(transform.position,0.5f,-transform.up,out RaycastHit hit,0.6f, 1 << 3);
+        // definition of the variables link to the layers
+        isGrounded = Physics.SphereCast(transform.position,0.5f,-transform.up,out RaycastHit groundHit,0.6f, 1 << 3);
+        isBouncing = Physics.SphereCast(transform.position, 0.5f, -transform.up, out RaycastHit bouncerHit, 0.6f, 1 << 6);
         if (isGrounded && playerVelocity.y < 0)
         {
             playerVelocity.y = -0.1f;
@@ -46,11 +49,16 @@ public class PlayerController : MonoBehaviour
         controller.Move(move * Time.deltaTime * playerSpeed);
         
         isJumping = inputManager.PlayerJumpedThisFrame();
+        // Player can jump if he is on layer ground (3)
         if (isJumping && isGrounded)
         {
             playerVelocity.y += Mathf.Sqrt(jumpHeight * -2.0f * gravityValue);
         }
-
+        // Player automatically jump if he is on layer bouncer (6)
+        if (isBouncing)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
         playerVelocity.y += gravityValue * Time.deltaTime;
         controller.Move(playerVelocity * Time.deltaTime);
 
