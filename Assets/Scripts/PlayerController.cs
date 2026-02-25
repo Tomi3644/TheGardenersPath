5dcf702f5d8bdc6bc65d2b412178369317bd7edd
Thomas OLIVON <tolivon07@gmail.com>
2026-02-25 15:34:24 +0100

    Bouncer bug fix & Ladder implementation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,8 +8,10 @@ public class PlayerController : MonoBehaviour
     private CharacterController controller;
     private Vector3 playerVelocity;
     private bool isGrounded;
-    private bool isBouncing;
+    private bool canBounce = true;
+    private bool bounced;
     private bool isJumping;
+    private bool isOnLadder;
     private InputManager inputManager;
     private Transform cameraTransform;
     private float gravityValue;
@@ -22,7 +26,8 @@ public class PlayerController : MonoBehaviour
     private float normalGravity;
     [SerializeField]
     private float fallGravity;
-
+    [SerializeField]
+    private float bounceHeight;
     private void Start()
     {
         gravityValue = normalGravity;
@@ -34,33 +39,53 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        // Definition of the variables link to the layers
-        isGrounded = Physics.SphereCast(transform.position,0.5f,-transform.up,out RaycastHit groundHit,0.6f, 1 << 3);
-        isBouncing = Physics.SphereCast(transform.position, 0.5f, -transform.up, out RaycastHit bouncerHit, 0.6f, 1 << 6);
-        // Player is affected by gravity
+        // Check if player on the ground
+        isGrounded = Physics.SphereCast(transform.position, 0.5f, -transform.up, out RaycastHit groundHit, 0.6f, 1 << 3);
+
         if (isGrounded && playerVelocity.y < 0)
         {
             playerVelocity.y = -0.1f;
         }
 
+        // Get player inputs and move accordingly
+        // If on ladder, player goes up
         Vector2 movement = inputManager.GetPlayerMovement();
         Vector3 move = new Vector3(movement.x, 0f, movement.y);
-        move = cameraTransform.forward * move.z + cameraTransform.right * move.x;
-        move.y = 0f;
-        controller.Move(move * Time.deltaTime * playerSpeed);
+        if (isOnLadder && movement.y > 0f)
+        {
+            move = cameraTransform.up * move.z + cameraTransform.right * move.x;
+        }
+        else
+        {
+            move = cameraTransform.forward * move.z + cameraTransform.right * move.x;
+            move.y = 0f;
+        }
         
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        // Player jump on input if on layer Ground
         isJumping = inputManager.PlayerJumpedThisFrame();
-        // Player can jump if he is on layer ground (3)
+
         if (isJumping && isGrounded)
         {
             playerVelocity.y += Mathf.Sqrt(jumpHeight * -2.0f * gravityValue);
         }
-        // Player automatically jump if he is on layer bouncer (6)
-        if (isBouncing)
+
+        // Player automatically jump if on layer Bouncer and cannot bounce twice in two frames
+        if (Physics.SphereCast(transform.position, 0.48f, -transform.up, out RaycastHit bouncerHit, 0.6f, 1 << 6))
         {
-            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+            if (canBounce && playerVelocity.y <= 0f)
+            {
+                playerVelocity.y = Mathf.Sqrt(bounceHeight * -2.0f * gravityValue);
+                canBounce = false;
+
+                StartCoroutine(BounceWait());
+            }
         }
-        playerVelocity.y += gravityValue * Time.deltaTime;
+
+        // Gravity application on player (different if on ladder)
+        if (isOnLadder && movement.y > 0f) playerVelocity.y += -0.5f * Time.deltaTime;
+        else playerVelocity.y += gravityValue * Time.deltaTime;
         controller.Move(playerVelocity * Time.deltaTime);
 
         var currentHeight = transform.position.y;
@@ -70,5 +95,28 @@ public class PlayerController : MonoBehaviour
         }
         else { gravityValue = normalGravity; }
         previousHeight = transform.position.y;
+    }
+
+    // Timer to prevent from bouncing twice in two frames
+    private IEnumerator BounceWait()
+    {
+        yield return new WaitForSeconds(.5f);
+        canBounce = true;
+    }
+
+    // Trigger handling for ladder
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Ladder")
+        {
+            isOnLadder = true;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Ladder")
+        {
+            isOnLadder = false;
+        }
     }
 }
