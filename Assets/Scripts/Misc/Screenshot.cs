6d22ede0dd053ee6d6617fa4b46b8cef66351d4c
GLaPI9999 <142686102+GLaPI9999@users.noreply.github.com>
2026-06-15 16:23:11 +0200

    set dress ext
    + screenshot sur P
    + modifs set dress post riviere

--- Assets/Scripts/Misc/Screenshot.cs
+++ Assets/Scripts/Misc/Screenshot.cs
@@ -1,21 +1,35 @@
 using UnityEngine;
+using System;
+using System.IO;
 
 public class Screenshot : MonoBehaviour
 {
-	[SerializeField]
-	private string path;
-	[SerializeField]
-	[Range(1, 5)]
-	private int size = 1;
-	
-	void Update()
-	{
-		if (Input.GetKeyDown(KeyCode.K))
-		{
-			path += "screenshot";
-			path += System.Guid.NewGuid().ToString() + ".png";
-			
-			ScreenCapture.CaptureScreenshot(path, size);
-		}
-	}
+    [SerializeField]
+    [Range(1, 5)]
+    private int size = 1;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            string picturesPath = Environment.GetFolderPath(
+                Environment.SpecialFolder.MyPictures);
+
+            string screenshotFolder = Path.Combine(
+                picturesPath,
+                "Gardener's Path");
+
+            Directory.CreateDirectory(screenshotFolder);
+
+            string fileName = $"screenshot_{Guid.NewGuid()}.png";
+
+            string fullPath = Path.Combine(
+                screenshotFolder,
+                fileName);
+
+            ScreenCapture.CaptureScreenshot(fullPath, size);
+
+            Debug.Log("Capture sauvegardée : " + fullPath);
+        }
+    }
 }
